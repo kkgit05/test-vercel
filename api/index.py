2d9f88eb474bb140f7409503ebeb6a8fb4c2850a
kkgit05 <gamingplacekk@gmail.com>
2025-04-07 10:38:24 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,1 +1,11 @@
-from src.index import app
+from fastapi import FastAPI, UploadFile, Form
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+
+@app.post("/")
+async def handle(question: str = Form(...), file: UploadFile = Form(...)):
+    return JSONResponse({
+        "question": question,
+        "filename": file.filename
+    })
